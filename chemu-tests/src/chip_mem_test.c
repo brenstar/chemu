eb3e7f649eacaf1dcbcd062b0f61152bf7885e69
brenstar <b.ringey@gmail.com>
2016-07-07 21:21:17 -0400

    Updated memory module
    Added functions to the memory module, chipmem_read and chipmem_write
    Rewrote the memory unit test program (chip_mem_test.c)
    Added documentation to the memory module

--- chemu-tests/src/chip_mem_test.c
+++ chemu-tests/src/chip_mem_test.c
@@ -1,23 +1,53 @@
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "chemu/memory.h"
 
+#define boolstr(x) x ? "true" : "false"
+
+typedef struct {
+    ChipAddress addr;
+    uint8_t value;
+    uint8_t expectedRead;
+    bool expectedWrite;
+} TestCase;
+
+static const TestCase TESTS[] = {
+    {0x200, 0xFF, 0xFF, true},
+    {0xFFF, 0x54, 0x54, true},
+    {0x1FF, 0xF0, 0x0, false}
+};
 
 int main() {
 
-    ChipMem *mem = chipmem_create();
+    ChipMem *mem = (ChipMem*)malloc(sizeof(ChipMem));
 
-    // print the entire fontset, with each digit (5 bytes) on one line
-    uint8_t *fontsetPtr = mem->reserved.fontset;
-    for (int i = 0; i < 16; ++i) {
-        for (int j = 0; j < 5; ++j)
-            printf("%02x ", *(fontsetPtr++));
-        printf("\n");
+    chipmem_init(mem);
+
+    printf("Checking fontset\n");
+    for (int i = 0; i < CHIPMEM_FONTSET_LEN; ++i)
+        assert(mem->array[i] == FONTSET[i]);
+
+    printf("Checking memory contents\n");
+    for (int i = CHIPMEM_FONTSET_LEN; i < CHIP_END; ++i)
+        assert(mem->array[i] == 0);
+
+    for (size_t i = 0; i < 3; ++i) {
+        TestCase test = TESTS[i];
+        printf("Writing 0x%02X at location 0x%03X...", test.value, test.addr);
+        bool result = chipmem_write(mem, test.addr, test.value);
+        printf("%s (expected: %s)\n", boolstr(result), boolstr(test.expectedWrite));
     }
 
-    chipmem_destroy(mem);
+    for (size_t i = 0; i < 3; ++i) {
+        TestCase test = TESTS[i];
+        printf("Reading from 0x%03X...", test.addr);
+        uint8_t result = chipmem_read(mem, test.addr);
+        printf("%02X (expected: %02X)\n", result, test.expectedRead);
+    }
 
+    free(mem);
 
     return 0;
 }
--- chemu/include/chemu/memory.h
+++ chemu/include/chemu/memory.h
@@ -5,19 +5,39 @@
 #include "ChipDP.h"
 #include "ChipAddress.h"
 
-//ChipMem* chipmem_create();
+#include <stdbool.h>
 
+/**
+ * Constant definition containing the fontset. The fontset contains 16 sprites,
+ * numbers 0-9 and letters A-F. Each sprite is 5 bytes long.
+ */
 extern const uint8_t FONTSET[];
 
+/**
+ * Initializes the ChipMem object. The fontset is copied into the reserved
+ * section at location 0x0 and the rest of the array is set to 0
+ */
 void chipmem_init(ChipMem *mem);
 
+/**
+ * Reads a byte at the given address from the given ChipMem object. Valid
+ * addresses range from 0 to 4095. The caller is responsible for checking the
+ * bounds of the addr parameter. If addr is greater than 4095, 0 is returned.
+ */
 uint8_t chipmem_read(ChipMem *mem, ChipAddress addr);
 
+/**
+ * Writes a given byte at the given address in the given ChipMem object. true
+ * is returned on sucessful write, false otherwise. False is returned if the
+ * addr parameter is between 0 and CHIPMEM_RESERVED_LEN (reserved portion) and
+ * the byte will not be written.
+ */
 bool chipmem_write(ChipMem *mem, ChipAddress addr, uint8_t value);
 
+/**
+ * Gets the address of the font sprite for the given digit. The digit
+ * parameter must be in the range of 0 to 15.
+ */
 ChipAddress chipmem_get_font(ChipMem *mem, uint8_t digit);
-
-
-//void chipmem_destroy(ChipMem *mem);
 
 #endif
